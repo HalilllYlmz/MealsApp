3df681d20353e769502f58b68d7c95f3711b225e
halilyilmaz <halilylm1903@gmail.com>
2025-12-09 21:26:55 +0300

    Implement MealsOverview screen and MealItem component; refactor CategoriesScreen

--- screens/MealsOverview.js
+++ screens/MealsOverview.js
@@ -1,22 +1,44 @@
-import { StyleSheet, Text, View } from "react-native";
+import { View, FlatList, StyleSheet } from "react-native";
 
-const MealsOverview = ({ route }) => {
-  const { categoryId } = route.params;
+import MealItem from "../components/MealItem";
+import { MEALS } from "../data/dummy-data";
+
+function MealsOverview({ route }) {
+  const catId = route.params.categoryId;
+
+  const displayedMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(catId) >= 0;
+  });
+
+  function renderMealItem(itemData) {
+    const item = itemData.item;
+
+    const mealItemProps = {
+      title: item.title,
+      imageUrl: item.imageUrl,
+      affordability: item.affordability,
+      complexity: item.complexity,
+      duration: item.duration,
+    };
+    return <MealItem {...mealItemProps} />;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Meals Overwiew Screen</Text>
-      <Text>Category ID: {categoryId}</Text>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
     </View>
   );
-};
+}
 
 export default MealsOverview;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    padding: 16,
   },
 });
